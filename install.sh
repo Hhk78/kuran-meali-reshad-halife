979eda7505313d92725966f8f7b221147c4e2237
Fedor or Not <84645312+Hhk78@users.noreply.github.com>
2023-07-25 07:32:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ curl -C - -L -O https://github.com/Hhk78/kuran-meali-reshad-halife/releases/down
 mkdir -p /home/$USER/.local/bin1
 mkdir -p /home/$USER/.local/reşad
 7z x resad.7z -o/home/$USER/.local/reşad
-cp reşad.sh /home/$USER/.local/bin1/reşad
-echo "export PATH=$PATH:/home/$USER/.local/bin:/home/$USER/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/$USER/.composer/vendor/bin:/home/$USER/bin:/opt/genymobile/genymotion:/home/$USER/.local/bin1/" >> /home/$USER/.zshrc
-echo "export PATH=$PATH:/home/$USER/.local/bin:/home/$USER/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/$USER/.composer/vendor/bin:/home/$USER/bin:/opt/genymobile/genymotion:/home/$USER/.local/bin1/" >> /home/$USER/.bashrc
+sudo cp reşad.sh /bin/reşad
+#echo "export PATH=$PATH:/home/$USER/.local/bin:/home/$USER/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/$USER/.composer/vendor/bin:/home/$USER/bin:/opt/genymobile/genymotion:/home/$USER/.local/bin1/" >> /home/$USER/.zshrc
+#echo "export PATH=$PATH:/home/$USER/.local/bin:/home/$USER/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/$USER/.composer/vendor/bin:/home/$USER/bin:/opt/genymobile/genymotion:/home/$USER/.local/bin1/" >> /home/$USER/.bashrc
